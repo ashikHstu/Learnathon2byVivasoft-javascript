774a36aa7f782a4e57f71f0e4b4d64d9751ca754
ashikur rahman <ashik.cse.hstu@gmail.com>
2023-09-15 16:57:12 +0600

    if-else and switch

---
+++ class2/flow_control_and_looping.js
@@ -0,0 +1,21 @@
+let a = 2;
+if (a == 2) {
+    console.log('test');
+}
+
+// switch
+
+switch (3) {
+    case 1:
+        console.log("the value is 1");
+        break;
+
+    case 4:
+        console.log("the value is 4");
+        break;
+    default:
+        console.log("nothing matches!");
+}
+
+
+
